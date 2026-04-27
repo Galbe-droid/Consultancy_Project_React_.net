4e859b5ef176124db9b980efed3380f92e70c4b7
Gabriel Lima <54965605+Galbe-droid@users.noreply.github.com>
2026-04-27 16:09:58 -0300

    Account Config

--- consultorFinanceiro-webapp/src/components/Modals/accountModal.tsx
+++ consultorFinanceiro-webapp/src/components/Modals/accountModal.tsx
@@ -1,4 +1,4 @@
-import {Box, Button, Dialog, Typography} from "@mui/material";
+import {Button, Dialog, DialogContent, DialogTitle} from "@mui/material";
 import {useAuth} from "../../hooks/useAuth.ts";
 
 
@@ -8,19 +8,18 @@ type Props = {
 };
 
 export function AccountModal({open, onClose}: Props) {
-    const {logout, deleteAccount, user} = useAuth();
+    const {deleteAccount, user} = useAuth();
 
     const deleteAccountSequence = async (id: string) => {
         deleteAccount(id);
-        logout()
     }
 
     return(
         <Dialog open={open} onClose={onClose}>
-            <Box sx={{display: "flex", flexDirection: "row", mb:4,gap: 2, ml:2, alignItems:"center",}}>
-                <Typography><strong>Deletar Conta:</strong> </Typography>
+            <DialogTitle><strong>Deletar Conta:</strong> </DialogTitle>
+            <DialogContent>
                 <Button onClick={() => (!confirm("Tem certeza que deseja deletar essa conta?") ? null : deleteAccountSequence(user?.id as string))} sx={(theme) => ({backgroundColor: theme.palette.error.main, color: theme.palette.common.black})}>Deletar</Button>
-            </Box>
+            </DialogContent>
         </Dialog>
     )
 }
